3ce05423c18787981b27c408c88f1825c9d935d2
amandasantiagu <amandynhah023@hotmail.com>
2020-04-28 20:48:51 -0300

    Implementaçao das rotas home e feed

--- NGBlogPessoal/src/app/app-routing.module.ts
+++ NGBlogPessoal/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { FeedComponent } from './feed/feed.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  /* Retorna para home quando estiver vazio COM TELA FULL*/
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'feed', component: FeedComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
