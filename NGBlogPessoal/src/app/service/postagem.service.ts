b274234f84b1da23e7c0710d4c925a3a2d33b3e1
amandasantiagu <amandynhah023@hotmail.com>
2020-04-29 22:05:30 -0300

    Criacao do endPOINT GET ALL POSTAGEN

--- NGBlogPessoal/src/app/service/postagem.service.ts
+++ NGBlogPessoal/src/app/service/postagem.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PostagemService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  /* 
+    CRUD - Create, READY, UPDATE E DELETE A 
+  */
+  getAllPostagens() {
+    return this.http.get('http://31.220.57.14:8080/postagens')
+
+  }
+
 }
